ae700f29303f1c09012b5b1cb367a8cc97b31387
Mohd Kaif <kaifmohd2014@gmail.com>
2023-03-05 22:59:24 +0530

    added otp screen and modified mainactivity

--- app/src/main/java/com/example/farmerbuddy/OtpActivity.java
+++ app/src/main/java/com/example/farmerbuddy/OtpActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class OtpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
+        setContentView(R.layout.activity_otp);
     }
 }
