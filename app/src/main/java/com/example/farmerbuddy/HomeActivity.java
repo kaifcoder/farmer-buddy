672b18878e5673389a9b5744ed27acc2ffe21477
Mohd Kaif <kaifmohd2014@gmail.com>
2023-03-07 19:05:19 +0530

    navigation setup

--- app/src/main/java/com/example/farmerbuddy/HomeActivity.java
+++ app/src/main/java/com/example/farmerbuddy/HomeActivity.java
@@ -1,14 +1,119 @@
 package com.example.farmerbuddy;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.farmerbuddy.ui.AboutFragment;
+import com.example.farmerbuddy.ui.CropsFragment;
+import com.example.farmerbuddy.ui.HomeFragment;
+import com.example.farmerbuddy.ui.NewsUpdates;
+import com.example.farmerbuddy.ui.ProfileFragment;
+import com.example.farmerbuddy.ui.Query;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar,R.string.open_nav,R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
+
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    navigationView.setCheckedItem(R.id.nav_home);
+                    break;
+                case R.id.crops:
+                    replaceFragment(new CropsFragment());
+                    break;
+                case R.id.govpolicy:
+                    replaceFragment(new NewsUpdates());
+                    break;
+                case R.id.ask:
+                    replaceFragment(new Query());
+                    break;
+            }
+
+            return true;
+        });
+
+    }
+
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                replaceFragment(new HomeFragment());
+
+                break;
+            case R.id.nav_profile:
+                replaceFragment(new ProfileFragment());
+                break;
+
+            case R.id.nav_about:
+                replaceFragment(new AboutFragment());
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(this,MainActivity.class);
+                startActivity(i);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/com/example/farmerbuddy/ProfileRegistration.java
+++ app/src/main/java/com/example/farmerbuddy/ProfileRegistration.java
@@ -2,13 +2,25 @@ package com.example.farmerbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileRegistration extends AppCompatActivity {
 
+
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_registration);
+        button = findViewById(R.id.proceed_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileRegistration.this,HomeActivity.class));
+            }
+        });
     }
 }
